d30fb5a8aae703477a3c2c6bb24fb95b70c3aaf6
Jo Wagner <jowagner@limago.de>
2023-06-09 09:23:32 +0200

    initial commit

--- 05_PWM/05_PWM/main.c
+++ 05_PWM/05_PWM/main.c
@@ -12,51 +12,19 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-ISR (INT0_vect)
-{
-	OCR0A = 64;
-	OCR2A = 64;
-}
-
-ISR (INT1_vect)
-{
-	OCR0A = 64 +128;
-	OCR2A = 64 +128;
-}
 
 int main(void)
 {
 	uint8_t weite = 0;
 	uint8_t richtung = 1;
-	DDRD |= (1 << DDD6);
+	DDRD |= (1 << DDD6) | (1 << DDD5);
 	// PD6 is now an output
 	
-	DDRB |= (1 << DDB3);
-	// PD6 is now an output
-	
-	DDRD &= ~(1 << DDD2);     // Clear the PD2 pin
-	// PD2 (PCINT0 pin) is now an input
 
-	PORTD |= (1 << PORTD2);    // turn On the Pull-up
-	// PD2 is now an input with pull-up enabled
-
-	DDRD &= ~(1 << DDD3);     // Clear the PD3 pin
-	// PD3 (PCINT1 pin) is now an input
-
-	PORTD |= (1 << PORTD3);    // turn On the Pull-up
-	// PD3 is now an input with pull-up enabled
-
-
-	
-	EICRA |= (1 << ISC00);    // set INT0 to trigger on ANY logic change
-	EIMSK |= (1 << INT0);     // Turns on INT0
-	
-	EICRA |= (1 << ISC10);    // set INT0 to trigger on ANY logic change
-	EIMSK |= (1 << INT1);     // Turns on INT0
-	
 	
 
 	OCR0A = 64+128;
+	OCR0B = 64;
 	// set PWM for 75% duty cycle
 
 
@@ -69,22 +37,17 @@ int main(void)
 	TCCR0B |= (1 << CS01);
 	// set prescaler to 8 and starts PWM
 	
-	OCR2A = 64+128;
-	// set PWM for 75% duty cycle
+	
 
 
-	TCCR2A |= (1 << COM0A1);
-	// set none-inverting mode
-
-	TCCR2A |= (1 << WGM00);
-	// set fast PC Mode
-
-	TCCR2B |= (1 << CS01);
-	// set prescaler to 8 and starts PWM
 
 	sei();
-	while (1);
+	while (1)
 	{
+		OCR0A = 64+128;	
+		_delay_ms(200);
+		OCR0A = 64;
+		_delay_ms(200);
 		
 	}
 }
